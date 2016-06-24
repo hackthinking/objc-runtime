13a44bba3f8477f1537cccc4fd21a54bc32e5e5a
phnessu4 <phnessu4@gmail.com>
2016-06-24 16:34:01 +0800

    添加测试，对比alloc 和allocWithZone

--- debug-objc/main.m
+++ debug-objc/main.m
@@ -8,10 +8,51 @@
 
 #import <Foundation/Foundation.h>
 
+@interface Subject : NSObject
++(instancetype)getInstance;
+
+@end
+
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
+        Subject *ia = [Subject getInstance];
+        
         // insert code here...
-        NSLog(@"Hello, World!");
+        NSLog(@"Hello, World! %@", ia);
     }
+    
     return 0;
 }
+
+
+@implementation Subject
+
+static Subject *sharedInstance = nil;
+
++(instancetype)getInstance
+{
+    @synchronized(self)
+    {
+        // 实例对象只分配一次
+        if(sharedInstance == nil)
+        {
+            //            sharedInstance = [[super allocWithZone:NULL] init];
+            sharedInstance = [[Subject alloc] init];
+            
+            // 初始化
+        }
+    }
+    return sharedInstance;
+}
+
+//+ (id)allocWithZone:(NSZone *)zone
+//{
+//    return [self getInstance];
+//}
+//
+//- (id)copyWithZone:(NSZone *)zone
+//{
+//    return self;
+//}
+
+@end
